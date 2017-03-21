79d508ab76198980f2c04bbe927017591e94ac7c
archiekey <archanakvenkatesh@gmail.com>
2017-03-20 17:01:01 -0700

    added to preprocessing

--- preprocesstest.py
+++ preprocesstest.py
@@ -1,5 +1,7 @@
 import glob 
 import numpy as np
+import re
+import preprocessor as p
 #http://www.aaai.org/ocs/index.php/ICWSM/ICWSM11/paper/viewFile/2886/3262
 # for name in glob.glob('tweet_log/*'):   # gets the filenames of the data text files
 #	print name
@@ -8,6 +10,8 @@ filenames = glob.glob('tweet_log/*')
 
 predata = []
 textdata = []
+cleaningdata = []
+testdata = []
 for ele in filenames:
 	with open(ele) as reading:
 		for line in reading:
@@ -19,6 +23,18 @@ for ele in filenames:
 
 for indata in predata:
 	textdata.append(indata[8:-1])
-	print(indata[8:-1])
 
+for hellodata in range(0,len(textdata)):
+	#print(textdata[hellodata])
+	tempphrase = re.sub(r'[^\x00-\x7F]',' ', textdata[hellodata])
+	newphrase = re.sub(r'#',' ', tempphrase)
+	cleaningdata.append(newphrase)
 
+p.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION,p.OPT.RESERVED,p.OPT.SMILEY)
+for pro in range(0,len(cleaningdata)):
+	testdata.append(p.clean(cleaningdata[pro]))
+	
+#for testing in range(0,len(testdata)):
+	#print testdata[testing]
+
+#testdata is the final output
