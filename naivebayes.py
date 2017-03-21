2480c7f9874c24e15d6f98e31c4af503fec0286f
archiekey <archanakvenkatesh@gmail.com>
2017-03-20 17:00:30 -0700

    added naivebayes algorithm

---
+++ naivebayes.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+
+from preprocesstest import testdata
+from preprocesstrainingdata import labels
+from preprocesstrainingdata import protrainingdata
+
+
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                                 stop_words='english')
+features_train_transformed = vectorizer.fit_transform(protrainingdata)
+features_test_transformed  = vectorizer.transform(testdata)
+
+
+bagvectorizer = CountVectorizer()
+
+bag_of_words = bagvectorizer.fit(protrainingdata)
+bag_of_words = bagvectorizer.transform(protrainingdata)
+
+clf = GaussianNB()
+
+
+fulldata = len(protrainingdata)
+halfdata = len(protrainingdata)/2
+newtrain = features_train_transformed[0:halfdata].toarray()
+#newtrain = bag_of_words[0:halfdata].toarray()
+clf.fit(newtrain ,labels[0:halfdata])
+
+pred = clf.predict(features_train_transformed[halfdata+1:fulldata].toarray())
+
+print accuracy_score(pred,labels[halfdata+1:fulldata])
